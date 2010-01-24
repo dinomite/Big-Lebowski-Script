14015329191cb9a3587d0b32f881c8dcdb4e7819
Drew Stephens <drew@dinomite.net>
2010-01-23 22:54:17 -0800

    Now with clean programming

--- lebowski.pl
+++ lebowski.pl
@@ -1,7 +1,12 @@
+#!/usr/bin/env perl
+# 2010, Drew Stephens <drew@dinomite.net>
+use warnings;
+use strict;
+
 open(FH, '<', 'big.lebowski.html') or die $!;
 
-$dialogRegex = qr/^ {25}[^ ]/;
-$style = "
+my $dialogRegex = qr/^ {25}[^ ]/;
+my $style = "
 <style>
     body { width: 30em }
     p { width: 25em }
@@ -9,44 +14,44 @@ $style = "
 </style>
 ";
 
-$count = 0;
-$blockquote = 0;
-while (<FH>) {
-    if (m#</HEAD>#) {
+my $count = 0;
+my $blockquote = 0;
+while (my $line = <FH>) {
+    if ($line =~ m#</HEAD>#) {
         print $style;
     }
 
-    if (/^ {15}[A-Z][ :A-Z']+$/) {
+    if ($line =~ /^ {15}[A-Z][ :A-Z']+$/) {
         # Scene title
-        s/^ +//;
-        s/^/<a name='$count'>/;
-        s/$/<\/a>/;
+        $line =~ s/^ +//;
+        $line =~ s/^/<a href='#$count' name='$count'>/;
+        $line =~ s/$/<\/a>/;
 
-        chomp $_;
-        print "<h2>$_</h2>\n";
+        chomp $line;
+        print "<h2>$line</h2>\n";
 
         $count++;
-    } elsif ($blockquote == 0 && /^ {37}[^ ]/) {
+    } elsif ($blockquote == 0 && $line =~ /^ {37}[^ ]/) {
         # Character dialog
-        s/^ +//;
-        chomp $_;
-        print "<b>$_</b>\n";
-    } elsif ($blockquote == 0 && $_ =~ $dialogRegex) {
+        $line =~ s/^ +//;
+        chomp $line;
+        print "<b>$line</b>\n";
+    } elsif ($blockquote == 0 && $line =~ $dialogRegex) {
         # Start of a dialog section
         print "<blockquote>\n";
-        print $_;
+        print $line;
 
         $blockquote = 1;
-    } elsif ($blockquote == 1 && $_ !~ $dialogRegex) {
+    } elsif ($blockquote == 1 && $line !~ $dialogRegex) {
         # End of a dialog section
         print "</blockquote>\n";
-        print $_;
+        print $line;
 
         $blockquote = 0;
-    } elsif (/^$/) {
+    } elsif ($line =~ /^$/) {
         print "</p>\n\n<p>\n";
-        print $_;
+        print $line;
     } else {
-        print $_;
+        print $line;
     }
 }
